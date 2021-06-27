8d4c0a8b85d2c147d4ee1122c123d9a7ec95eb76
DESKTOP\Yatong <mike.z@anzuk.education>
2021-06-27 20:57:58 +1000

    Add IOC and load appsetting

---
+++ App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuppliesPriceLister
+{
+    public class App
+    {
+        private readonly IConfigurationRoot _config;
+        private readonly ILogger<App> _logger;
+
+        public App(IConfigurationRoot config, ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<App>();
+            _config = config;
+        }
+
+        public async Task Run()
+        {
+            Console.WriteLine("first line");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,83 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace SuppliesPriceLister
 {
     class Program
     {
+        public static IConfigurationRoot configuration;
+
         static void Main(string[] args)
         {
-            // Your solution begins here
+            // Initialize serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo.File("log.txt")
+                 .MinimumLevel.Debug()
+                 .Enrich.FromLogContext()
+                 .CreateLogger();
+
+            MainAsync(args).Wait();
+
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            // Create service collection
+            Log.Information("Creating service collection");
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Create service provider
+            Log.Information("Building service provider");
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Print connection string to demonstrate configuration object is populated
+            Console.WriteLine(configuration.GetConnectionString("DataConnection"));
+
+            try
+            {
+                Log.Information("Starting service");
+                await serviceProvider.GetService<App>().Run();
+                Log.Information("Ending service");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error running service");
+                throw ex;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Add logging
+            serviceCollection.AddSingleton(LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddSerilog(dispose: true);
+            }));
+
+            serviceCollection.AddLogging();
+
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            // Add app
+            serviceCollection.AddTransient<App>();
         }
     }
 }
